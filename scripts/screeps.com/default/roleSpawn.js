90c887f9e8ee4f2e47e2ed505118875c492c27c6
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-04 02:37:24 -0600

    stop attempting to spawn when not enough energy for body

--- scripts/screeps.com/default/roleSpawn.js
+++ scripts/screeps.com/default/roleSpawn.js
@@ -1,7 +1,19 @@
+const { creepTypes } = require('./creepTypes');
+const { bodyCost } = require('./creepCommon');
+
 function isFull(type) {
     if(countCreepsOfType(type) >= type.maxCount)
         return true;
     return false;
+}
+
+function spawnType(spawner, type) {
+    const creepNumber = Math.floor(Math.random() * 10000);
+    const creepName = `creep${creepNumber}`;
+    const spawnResult = spawner.spawnCreep(type.body, creepName, { memory: type.memory } );
+    const string = (spawnResult === 0) ? 'success' : `fail with code ${spawnResult}`;
+    console.log('Attempting to spawn creep of type', type.memory.role, `with name: ${creepName}...${string}`);
+    logCreepCounts();
 }
 
 const roleSpawn = {
@@ -9,26 +21,30 @@ const roleSpawn = {
         if(spawner.spawning)
             return;
         
-        if(spawner.store[RESOURCE_ENERGY] < 300)
+        const currentEnergy = spawner.store[RESOURCE_ENERGY];
+        if(currentEnergy < 300)
+            return;
+
+        const typeVals = Object.values(creepTypes);
+        if(typeVals.length < 1)
             return;
         
-        for(const typeKey of Object.keys(creepTypes)){
-            const type = creepTypes[typeKey];
-            
-            if(isFull(type))
-                continue;
-            
-            this.spawnType(spawner, type);
-            logCreepCounts();
+        updateCurrentCreepCounts();
+
+        typeVals.sort((a,b) => {
+            const aManningFraction = a.currentCount / a.maxCount;
+            const bManningFraction = b.currentCount / b.maxCount;
+            return aManningFraction - bManningFraction;
+        });
+        const typeToBuild = typeVals[0];
+        if(isFull(typeToBuild))
             return;
-        }
-    },
-    spawnType: function(spawner, type){
-        const creepNumber = Math.floor(Math.random() * 10000);
-        const creepName = `creep${creepNumber}`;
-        const spawnResult = spawner.spawnCreep(type.body, creepName, { memory: type.memory } );
-        const string = (spawnResult === 0) ? 'success' : `fail with code ${spawnResult}`;
-        console.log('Attempting to spawn creep of type', type.memory.role, `with name: ${creepName}`, '...' + string);
+
+        if(currentEnergy < bodyCost)
+            return;
+        
+        spawnType(spawner, typeToBuild);
+        return;
     },
 };
 module.exports = { roleSpawn };
