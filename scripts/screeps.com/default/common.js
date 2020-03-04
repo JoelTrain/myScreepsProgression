db667c668d8aff18e0d2291e4923f89cef65948f
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-04 02:30:13 -0600

    added random list picker

---
+++ scripts/screeps.com/default/common.js
@@ -0,0 +1,10 @@
+function pickRandomFromList(list) {
+    if(!list.length) {
+        throw Error(`Cannot pick at random from ${List}`);
+    }
+
+    const index = Math.floor(Math.random() * list.length);
+    return list[index];
+}
+
+module.exports = { pickRandomFromList };
