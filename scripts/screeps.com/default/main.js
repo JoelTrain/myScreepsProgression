52c4f7c8235d0b801a9949022aca36889d02704a
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-04 12:48:49 -0600

    append the current time to exceptions to make emails easier to read

--- scripts/screeps.com/default/main.js
+++ scripts/screeps.com/default/main.js
@@ -6,35 +6,41 @@ const { roleManual } = require('./roleManual');
 const { roleSpawn } = require('./roleSpawn');
 
 function main() {
-    const time = Game.time;
-    for(var i in Memory.creeps) {
-        const creep = Game.creeps[i];
-        if(!creep) {
-            delete Memory.creeps[i];
-            continue;
-        }
+    try {
+        const time = Game.time;
+        for(var i in Memory.creeps) {
+            const creep = Game.creeps[i];
+            if(!creep) {
+                delete Memory.creeps[i];
+                continue;
+            }
 
-        if(creep.memory._move && time > creep.memory._move.time)
-            delete creep.memory._move;
+            if(creep.memory._move && time > creep.memory._move.time)
+                delete creep.memory._move;
+        }
+        for(const creepHash in Game.creeps){
+            const creep = Game.creeps[creepHash];
+            
+            if(creep.memory.role === 'harvester')
+                roleHarvester.run(creep);
+                
+            if(creep.memory.role === 'builder')
+                roleBuilder.run(creep);
+                
+            if(creep.memory.role === 'upgrader')
+                roleUpgrader.run(creep);
+                
+            if(creep.memory.role === 'manual')
+                roleManual.run(creep);
+        }
+        for(const spawnHash in Game.spawns){
+            const spawn = Game.spawns[spawnHash];
+            roleSpawn.run(spawn);
+        }
     }
-    for(const creepHash in Game.creeps){
-        const creep = Game.creeps[creepHash];
-        
-        if(creep.memory.role === 'harvester')
-            roleHarvester.run(creep);
-            
-        if(creep.memory.role === 'builder')
-            roleBuilder.run(creep);
-            
-        if(creep.memory.role === 'upgrader')
-            roleUpgrader.run(creep);
-            
-        if(creep.memory.role === 'manual')
-            roleManual.run(creep);
-    }
-    for(const spawnHash in Game.spawns){
-        const spawn = Game.spawns[spawnHash];
-        roleSpawn.run(spawn);
+    catch (error) {
+        error.message = error.message.concat(' at time:', currentTimeString());
+        throw error;
     }
 }
 
