722d29fd56f5c323ce937edafc2a5c1f46e367f9
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-04 02:34:25 -0600

    manual role uses state machine to move to flag, capture then harvest

--- scripts/screeps.com/default/roleManual.js
+++ scripts/screeps.com/default/roleManual.js
@@ -1,6 +1,47 @@
+const { activitySetup, changeActivity } = require('./activity');
+
 const roleManual = {
     run: function(creep) {
-        creep.moveTo(Game.flags['Flag1'], { visualizePathSyle: {} });
+        activitySetup(creep);
+        
+        this[creep.memory.activity](creep);
+    },
+    'default': function(creep) {
+        changeActivity(creep, 'moving to flag');
+    },
+    'moving to flag': function(creep) {
+        const flag = Game.flags['Flag1'];
+        if(creep.pos.inRangeTo(flag, 0)){ 
+            changeActivity(creep, 'moving to foreign controller');
+            return;
+        }
+
+        creep.moveTo(flag, { visualizePathStyle: {} });
+    },
+    'moving to foreign controller': function(creep) {
+        if(creep.pos.inRangeTo(creep.room.controller, 1)) {
+            changeActivity(creep, 'claiming controller');
+            return;
+        }
+
+        creep.moveTo(creep.room.controller, { visualizePathStyle: {} });
+    },
+    'claiming controller': function(creep) {
+        if(creep.room.controller.my) {
+            creep.say('Claimed!');
+            changeActivity(creep, 'change to harvester');
+            return;
+        }
+
+        const failCode = creep.claimController(creep.room.controller);
+        if(failCode) {
+            console.log("Failed with code", failCode);
+            changeActivity(creep, 'change to harvester');
+        }
+    },
+    'change to harvester': function(creep) {
+        creep.memory.role = 'harvester';
+        changeActivity(creep, 'default');
     },
 }
 
