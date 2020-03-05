559030a20884f096feae150e5a7c7667bb5c991c
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-05 02:13:44 -0600

    added common activities to activity

--- scripts/screeps.com/default/activity.js
+++ scripts/screeps.com/default/activity.js
@@ -1,10 +1,18 @@
 const { pickRandomFromList } = require('./common');
+const {
+    creepIsEmpty,
+    creepIsFull,
+} = require('./creepCommon');
 
 let callOuts = false;
 
 function activitySetup(creep) {
     if(creep.memory.activity === undefined)
-        changeActivity(creep, 'default');
+        creep.memory.activity = 'default';
+    if(creep.memory.whenEmpty === undefined)
+        creep.memory.whenEmpty = 'searching for source';
+    if(creep.memory.whenFull === undefined)
+        creep.memory.whenFull = 'moving to controller';
 }
 
 function changeActivity(creep, newActivity) {
@@ -21,7 +29,33 @@ function changeActivityToRandomPickFromList(creep, activityList) {
     changeActivity(creep, pickRandomFromList(activityList));
 }
 
+const activity = {
+    'moving to controller': function(creep) {
+        const controller = creep.room.controller;
+        if(creep.pos.inRangeTo(controller, 3)) {
+            changeActivity(creep, 'upgrading controller');
+            return;
+        }
+        creep.moveTo(controller, { visualizePathStyle: {} });
+    },
+    'upgrading controller': function(creep) {
+        if(creepIsEmpty(creep)) {
+            changeActivity(creep, creep.memory.whenEmpty);
+            return;
+        }
+
+        const controller = creep.room.controller;
+        const result = creep.upgradeController(controller);
+
+        if(result !== OK){
+            changeActivity(creep, 'default');
+            return;
+        }
+    }
+};
+
 module.exports = { 
+    activity,
     activitySetup,
     changeActivity, 
     changeActivityToRandomPickFromList,
