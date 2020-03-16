769d719b444eaf6e5822cb8bd0611d31f21043e7
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-16 00:06:54 -0500

    add healing to move to rally point

--- scripts/screeps.com/experimental/activityMoveToRallyPoint.js
+++ scripts/screeps.com/experimental/activityMoveToRallyPoint.js
@@ -1,16 +1,33 @@
 const { moveIgnore } = require('./moveIgnore');
 const { changeActivity } = require('./changeActivity');
+const { isAlly } = require('./isAlly');
 
 function activityMoveToRallyPoint(creep) {
+  if (creep.getActiveBodyparts(HEAL)) {
+    creep.heal(creep);
+
+    let healingAnAlly = false;
+    const friendly = creep.pos.findClosestByRange(FIND_CREEPS, {
+      filter: function (object) {
+        return isAlly(object) && object.hits < object.hitsMax;
+      }
+    });
+    if (friendly) {
+      if (creep.pos.isNearTo(friendly)) {
+        creep.heal(friendly);
+        healingAnAlly = true;
+      }
+      else if (creep.pos.inRangeTo(friendly, 3)) {
+        creep.rangedHeal(friendly);
+        healingAnAlly = true;
+      }
+    }
+  }
   const rallyTarget = Game.flags[creep.memory.rallyPoint];
   if (rallyTarget) {
-    if (creep.pos.inRangeTo(rallyTarget, 5)) {
+    if (creep.pos.inRangeTo(rallyTarget, 0)) {
       if (creep.memory.rallyPoint2) {
-        const nextStep = Game.flags[creep.memory.rallyPoint2];
-        if(nextStep) {
-            creep.memory.targetPos = nextStep.pos;
-            changeActivity(creep, 'move to position')
-        }
+        creep.memory.rallyPoint = creep.memory.rallyPoint2;
       }
       else {
         changeActivity(creep, creep.memory.whenEmpty);
@@ -19,9 +36,11 @@ function activityMoveToRallyPoint(creep) {
     }
     moveIgnore(creep, rallyTarget, { reusePath: 20, visualizePathStyle: { stroke: 'yellow' } });
   }
-  else {
+
+
+
+  if (!rallyTarget) {
     changeActivity(creep, creep.memory.whenEmpty);
-    return;
   }
 }
 
