16ea61d459c9129029f2080b0fe9bbd6b32dc635
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-16 00:07:08 -0500

    add healing to move to target

--- scripts/screeps.com/experimental/activityMoveToTarget.js
+++ scripts/screeps.com/experimental/activityMoveToTarget.js
@@ -1,7 +1,28 @@
 const { moveIgnore } = require('./moveIgnore');
 const { changeActivity } = require('./changeActivity');
+const { isAlly } = require('./isAlly');
 
 function activityMoveToTarget(creep) {
+  if (creep.getActiveBodyparts(HEAL)) {
+    creep.heal(creep);
+
+    let healingAnAlly = false;
+    const friendly = creep.pos.findClosestByRange(FIND_CREEPS, {
+      filter: function (object) {
+        return isAlly(object) && object.hits < object.hitsMax;
+      }
+    });
+    if (friendly) {
+      if (creep.pos.isNearTo(friendly)) {
+        creep.heal(friendly);
+        healingAnAlly = true;
+      }
+      else if (creep.pos.inRangeTo(friendly, 3)) {
+        creep.rangedHeal(friendly);
+        healingAnAlly = true;
+      }
+    }
+  }
   const rallyTarget = creep.memory.targetPos;
   if (rallyTarget) {
     if (creep.pos.inRangeTo(rallyTarget, 1)) {
