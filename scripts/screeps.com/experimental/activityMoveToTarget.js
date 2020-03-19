df0eb9d54255e18113734d40da056a79b3c83420
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-03-19 11:13:32 -0500

    fix move to target

--- scripts/screeps.com/experimental/activityMoveToTarget.js
+++ scripts/screeps.com/experimental/activityMoveToTarget.js
@@ -22,11 +22,13 @@ function activityMoveToTarget(creep) {
   }
   const rallyTarget = creep.memory.targetPos;
   if (rallyTarget) {
-    if (creep.pos.inRangeTo(rallyTarget, 1)) {
+    const { roomName, x, y } = rallyTarget;
+    const roomPosition = new RoomPosition(x, y, roomName);
+    if (creep.pos.inRangeTo(roomPosition, 1)) {
       changeActivity(creep, creep.memory.whenEmpty);
       return;
     }
-    moveIgnore(creep, { pos: rallyTarget }, { reusePath: 20, visualizePathStyle: { stroke: 'yellow' } });
+    moveIgnore(creep, { pos: roomPosition }, { reusePath: 20, visualizePathStyle: { stroke: 'yellow' } });
   }
   else {
     changeActivity(creep, creep.memory.whenEmpty);
