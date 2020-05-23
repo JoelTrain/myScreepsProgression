8a126336c328f3b05fa4f767312d1f80092e307b
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-05-23 00:49:50 -0500

    add param for only my structures

--- scripts/screeps.com/experimental/findStructuresWithFreeSpace.js
+++ scripts/screeps.com/experimental/findStructuresWithFreeSpace.js
@@ -1,6 +1,8 @@
-function findStructuresWithFreeSpace(room, structure_type) {
+function findStructuresWithFreeSpace(room, structure_type, onlyMine = true) {
   const structures = room.find(FIND_STRUCTURES, {
     filter: function (object) {
+      if (onlyMine && !object.my)
+        return false;
       return object.structureType === structure_type && object.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
     }
   });
