ad91b28eb9e080352d174aff30717636cea99a04
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-05-23 00:50:46 -0500

    replace specifics with generic

--- scripts/screeps.com/experimental/findExtensionsWithFreeSpace.js
+++
@@ -1,7 +0,0 @@
-function findExtensionsWithFreeSpace(room) {
-  return room.find(FIND_MY_STRUCTURES, {
-    filter: structure => structure.structureType == STRUCTURE_EXTENSION && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-  });
-}
-
-module.exports = { findExtensionsWithFreeSpace };
--- scripts/screeps.com/experimental/findTransferTargets.js
+++ scripts/screeps.com/experimental/findTransferTargets.js
@@ -1,20 +1,17 @@
-const { findExtensionsWithFreeSpace } = require('./findExtensionsWithFreeSpace');
 const { findTerminalWithLessThan15kEnergy } = require('./findTerminalWithLessThan15kEnergy');
 const { findStructuresWithFreeSpace } = require('./findStructuresWithFreeSpace');
-const { findMySpawnsWithFreeSpace } = require('./findMySpawnsWithFreeSpace');
-const { findStorageWithFreeSpace } = require('./findStorageWithFreeSpace');
-const { findTowersWithFreeSpace } = require('./findTowersWithFreeSpace');
 
 function findTransferTargets(creep) {
   let targets = [];
-  targets.push(...findTowersWithFreeSpace(creep.room));
-  targets.push(...findExtensionsWithFreeSpace(creep.room));
-  targets.push(...findMySpawnsWithFreeSpace(creep.room));
+  targets.push(...findStructuresWithFreeSpace(creep.room, STRUCTURE_TOWER));
+  targets.push(...findStructuresWithFreeSpace(creep.room, STRUCTURE_EXTENSION));
+  targets.push(...findStructuresWithFreeSpace(creep.room, STRUCTURE_SPAWN));
   targets.push(...findTerminalWithLessThan15kEnergy(creep.room));
-  // targets.push(...findStructuresWithFreeSpace(creep, STRUCTURE_TERMINAL));
   // causes problem with pickup and immediatly transfer back
-  //targets.push(...findStorageWithFreeSpace(creep));
+  //targets.push(...findStructuresWithFreeSpace(creep.room, STRUCTURE_STORAGE));
 
+  // for fast stock up on energy in terminal, uncomment next line
+  // targets.push(...findStructuresWithFreeSpace(creep, STRUCTURE_TERMINAL));
   return targets;
 }
 
