34aa4c8671c6418833f0bf44c12c9919042228d7
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-07-26 01:05:56 -0500

    refactored attack to melee over heal

--- scripts/screeps.com/experimental/activityAttack.js
+++ scripts/screeps.com/experimental/activityAttack.js
@@ -5,26 +5,14 @@ function activityAttack(creep) {
   let targets = [];
 
   let healingAnAlly = false;
-  const friendly = creep.pos.findClosestByRange(FIND_CREEPS, {
-    filter: function (object) {
-      return isAlly(object) && object.hits < object.hitsMax;
-    }
-  });
-  if (friendly) {
-    if (creep.pos.isNearTo(friendly)) {
-      creep.heal(friendly);
-      healingAnAlly = true;
-    }
-    else if (creep.pos.inRangeTo(friendly, 3)) {
-      creep.rangedHeal(friendly);
-      healingAnAlly = true;
-    }
-    else if (creep.hits === creep.hitsMax)
-      healingAnAlly = true;
-  }
+  const attackPartCount = creep.getActiveBodyparts(ATTACK);
+  const rangedPartCount = creep.getActiveBodyparts(RANGED_ATTACK);
+  const totalAttackPartCount = attackPartCount + rangedPartCount;
+  const healPartCount = creep.getActiveBodyparts(HEAL);
+  const claimPartCount = creep.getActiveBodyparts(CLAIM);
 
   const controller = creep.room.controller;
-  if (controller && !controller.my)
+  if (controller && !controller.my && claimPartCount > 0)
     if (creep.pos.inRangeTo(controller, 1)) {
       if (controller.reservation && controller.reservation.username !== 'ComradeJoecool') {
         creep.attackController(controller);
@@ -55,7 +43,7 @@ function activityAttack(creep) {
   }
   if (attackCreepsInThisRoom) {
     const enemyStructure = creep.room.lookForAt(LOOK_STRUCTURES, attackMovePos)[0];
-    if (enemyStructure && !isAlly(enemyStructure))
+    if (enemyStructure && !isAlly(enemyStructure) && enemyStructure.structureType != STRUCTURE_CONTROLLER)
       targets.push(enemyStructure);
 
     const enemy = creep.room.lookForAt(LOOK_CREEPS, attackMovePos)[0];
@@ -85,30 +73,63 @@ function activityAttack(creep) {
     targets = creep.room.find(FIND_HOSTILE_CREEPS, { filter: !isAlly });
   }
 
-  if (targets.length) {
+  let isMeleeAttacking = false;
+  let isRangedAttacking = false;
+
+  if (targets.length && totalAttackPartCount) {
     const target = creep.pos.findClosestByRange(targets);
     if (target) {
-      if (target instanceof Creep && creep.pos.inRangeTo(target, 3) && target.getActiveBodyparts(RANGED_ATTACK) === 0 && target.getActiveBodyparts(ATTACK) > 0) {
-        const pathResult = PathFinder.search(creep.pos, { pos: target.pos, range: 2 }, { flee: true });
-        console.log(pathResult);
-        if (pathResult && pathResult.path.length)
-          moveIgnore(creep, pathResult.path[0]);
-        const attackResult = creep.attack(target);
-        if (attackResult === ERR_NOT_IN_RANGE && !healingAnAlly && creep.hits < creep.hitsMax)
-          creep.heal(creep);
-        creep.rangedAttack(target);
-        return;
+      if (target instanceof Creep && creep.pos.inRangeTo(target, 3) && rangedPartCount > 0) {
+        isRangedAttacking = creep.rangedAttack(target) === OK;
+        isMeleeAttacking = creep.attack(target) === OK;
+
+        if (target.getActiveBodyparts(RANGED_ATTACK) === 0 && target.getActiveBodyparts(ATTACK) > 0) {
+          // run just out of range if they cannot melee
+          const pathResult = PathFinder.search(creep.pos, { pos: target.pos, range: 2 }, { flee: true });
+          console.log(pathResult);
+          if (pathResult && pathResult.path.length)
+            moveIgnore(creep, pathResult.path[0]);
+          if (!isMeleeAttacking && !healingAnAlly && creep.hits < creep.hitsMax)
+            creep.heal(creep);
+          //could also heal my neighbors here
+          return;
+        }
       }
-      else if (!creep.pos.inRangeTo(target, 1)) {
+      if (!creep.pos.inRangeTo(target, 1) && attackPartCount > 0) {
         moveIgnore(creep, target.pos, { visualizePathStyle: { stroke: 'red' } });
         if (!healingAnAlly && creep.hits < creep.hitsMax)
           creep.heal(creep);
       }
-      else
-        creep.attack(target);
-      creep.rangedAttack(target);
       return;
     }
+  }
+
+  if (!isMeleeAttacking
+    && !isRangedAttacking
+    && healPartCount > 0) {
+    const friendly = creep.pos.findClosestByRange(FIND_CREEPS, {
+      filter: function (object) {
+        return isAlly(object) && object.hits < object.hitsMax;
+      }
+    });
+    if (friendly) {
+      if (creep.pos.isNearTo(friendly)) {
+        creep.heal(friendly);
+        healingAnAlly = true;
+      }
+      else if (creep.pos.inRangeTo(friendly, 3)) {
+        creep.rangedHeal(friendly);
+        healingAnAlly = true;
+      }
+      else if (creep.hits === creep.hitsMax)
+        healingAnAlly = true;
+    }
+  }
+
+  if (isMeleeAttacking
+    || isRangedAttacking
+    || healingAnAlly) {
+    return;
   }
 
   if (attackMovePos) {
