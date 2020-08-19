2c8279868bc0a05581ea53f7fbc177451638ad37
JoelTrain <2225085+JoelTrain@users.noreply.github.com>
2020-08-18 23:44:16 -0500

    fixed drop and pickup oxygen issue

--- scripts/screeps.com/experimental/activityDepositIntoStorage.js
+++ scripts/screeps.com/experimental/activityDepositIntoStorage.js
@@ -7,6 +7,9 @@ function activityDepositIntoStorage(creep) {
     changeActivity(creep, creep.memory.whenEmpty);
   }
 
+  // beware of downgraded rooms with terminals
+  // terminals can have 0 maximum capacity (divide by 0 issues)
+
   let dropoffStructureTypes = [STRUCTURE_STORAGE];
   if (creep.store[RESOURCE_OXYGEN] > 0)
     dropoffStructureTypes = [STRUCTURE_TERMINAL];
@@ -16,6 +19,10 @@ function activityDepositIntoStorage(creep) {
   const storages = creep.room.find(FIND_MY_STRUCTURES, { filter: object => object.structureType === STRUCTURE_STORAGE });
   if (storages.length === 0 || (storages[0].store.getFreeCapacity(RESOURCE_ENERGY) / storages[0].store.getCapacity(RESOURCE_ENERGY) < 0.05))
     dropoffStructureTypes.push(STRUCTURE_TERMINAL);
+
+  const terminals = creep.room.find(FIND_MY_STRUCTURES, { filter: object => object.structureType === STRUCTURE_TERMINAL });
+  if (terminals.length === 0 || (terminals[0].store.getFreeCapacity(RESOURCE_ENERGY) <= 0) || (terminals[0].store.getFreeCapacity(RESOURCE_ENERGY) / terminals[0].store.getCapacity(RESOURCE_ENERGY) < 0.05))
+    dropoffStructureTypes.push(STRUCTURE_STORAGE);
 
   let target;
 
@@ -35,6 +42,7 @@ function activityDepositIntoStorage(creep) {
     }
     // drop all resources
     for (const resourceType in creep.carry) {
+      console.log(creep.name, 'dropping');
       creep.drop(resourceType);
       break;
     }
