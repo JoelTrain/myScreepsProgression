b2f1bbccfbc877402f6d9401977707fe02cc16f2
JoelTrain <jbirlingmair@gmail.com>
2022-02-21 22:34:17 -0600

    more creepTypeBuilder sizes

--- scripts/screeps.com/experimental/creepTypeBuilder.js
+++ scripts/screeps.com/experimental/creepTypeBuilder.js
@@ -1,6 +1,39 @@
 const { workCarryMoveBody } = require('./workCarryMoveBody');
 
-const creepTypeBuilder = {
+const tiny = {
+  body: workCarryMoveBody(8),
+  memory: {
+    role: 'builder',
+    activity: 'withdraw',
+    whenFull: 'build',
+    whenEmpty: 'withdraw',
+  },
+  maxCount: 3,
+};
+
+const small = {
+  body: workCarryMoveBody(12),
+  memory: {
+    role: 'builder',
+    activity: 'withdraw',
+    whenFull: 'build',
+    whenEmpty: 'withdraw',
+  },
+  maxCount: 3,
+};
+
+const medium = {
+  body: workCarryMoveBody(8),
+  memory: {
+    role: 'builder',
+    activity: 'withdraw',
+    whenFull: 'build',
+    whenEmpty: 'withdraw',
+  },
+  maxCount: 3,
+};
+
+const large = {
   body: workCarryMoveBody(16),
   memory: {
     role: 'builder',
@@ -9,6 +42,25 @@ const creepTypeBuilder = {
     whenEmpty: 'withdraw',
   },
   maxCount: 3,
+};
+
+const max = {
+  body: workCarryMoveBody(16),
+  memory: {
+    role: 'builder',
+    activity: 'withdraw',
+    whenFull: 'build',
+    whenEmpty: 'withdraw',
+  },
+  maxCount: 3,
+};
+
+const creepTypeBuilder = {
+  tiny: tiny,
+  small: small,
+  medium: medium,
+  large: large,
+  max: max,
 };
 
 module.exports = { creepTypeBuilder };
